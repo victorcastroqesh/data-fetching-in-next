df8a1a7008aa07d4af25c16dee169f90a1114090
Victor Castro <victor.castro@qesh.ai>
2022-10-24 18:55:16 -0300

    :tada:

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+const Home = () => {
+  return (
+    <>
+      <h1>Fetching Data</h1>
+      <ul>
+        <li>
+          getStaticProps :<Link href={"/about"}>About Page</Link>
+        </li>
+        <li>
+          getStaticPaths :<Link href={"/users/1"}>User 1</Link>
+        </li>
+        <li>
+          getServerSideProps :<Link href={"/posts"}>Posts Page</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Home;

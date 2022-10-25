0b9aad7a5caac3fa88c74fc539e2f46fa09ff8c6
Victor Castro <victor.castro@qesh.ai>
2022-10-25 10:03:05 -0300

    :sparkles: albums and posts

--- pages/albums.jsx
+++ pages/albums.jsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div></div>;
+import React from 'react'
+
+export const getStaticProps = async () => {
+
+	const res = await fetch(
+		'https://jsonplaceholder.typicode.com/albums');
+  let allAlbums = await res.json();
+  
+	return {
+		props: {
+			allAlbums: allAlbums.map((album) => album.title)
+		}
+	}
 }
+
+const Albums = ({ allAlbums }) => {
+	return (
+		<div>
+			<h1>All Albums</h1>
+			{allAlbums.map((album, idx) => (
+				<div key={idx}>{album}</div>))
+			}
+		</div>
+	)
+}
+
+export default Albums
--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div></div>;
+import React from 'react'
+
+export const getServerSideProps = async (context) => {
+
+	const res = await fetch(
+		'https://jsonplaceholder.typicode.com/posts');
+	let allPosts = await res.json();
+
+	return {
+		props: {
+			allPosts: allPosts.map((post) => post.title)
+		}
+	}
 }
+
+const Posts = ({ allPosts }) => {
+	return (
+		<div>
+			<h1>All Posts</h1>
+			{allPosts.map((post, idx) => (
+				<div key={idx}>{post}</div>))}
+		</div>
+	)
+}
+
+export default Posts
